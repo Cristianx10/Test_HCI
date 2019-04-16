ece43f9b2987c66483d93a0013ede39a6bad07af
Cristian Salguero <david_salinasx10@hotmail.com>
2019-04-16 10:27:21 -0500

    Cortes

--- app/js/cortes.js
+++ app/js/cortes.js
@@ -12,24 +12,141 @@ function degrees(radians) {
 function raizN(x, n) {
     return Math.exp(Math.log(x) / n);
 }
+var salaCirugia = /** @class */ (function () {
+    function salaCirugia() {
+        var _this = this;
+        this.canvas = document.createElement('canvas');
+        this.sala = new createjs.Stage(this.canvas);
+        this.cortando = false;
+        this.canvas.width = 1000;
+        this.canvas.height = 480;
+        this.canvas.style.background = "#AEE9DB";
+        var bisturi_img = document.createElement('img');
+        bisturi_img.src = "../img/cortes/bisturi.png";
+        var corte = new createjs.Shape();
+        this.sala.addChild(corte);
+        bisturi_img.addEventListener("load", function () {
+            _this.bisturi = new createjs.Bitmap(bisturi_img);
+            _this.sala.addChild(_this.bisturi);
+            _this.sala.on("stagemousemove", function () {
+                var x = _this.sala.mouseX;
+                var y = _this.sala.mouseY;
+                if (_this.bisturi != null) {
+                    _this.bisturi.x = x - 5;
+                    _this.bisturi.y = y;
+                }
+                if (_this.cortando) {
+                    corte.graphics.beginFill("red").drawCircle(x, y, 1);
+                }
+                _this.sala.update();
+            });
+        });
+    }
+    salaCirugia.prototype.cargarCuerpo = function (url) {
+        var _this = this;
+        var cuerpoA = document.createElement('img');
+        cuerpoA.src = url;
+        cuerpoA.addEventListener("load", function () {
+            var ima = new createjs.Bitmap(cuerpoA);
+            var cor = ima.getBounds();
+            ima.x = (_this.canvas.width - cor.width) / 2;
+            _this.sala.addChildAt(ima, 0);
+            _this.sala.update();
+        });
+    };
+    return salaCirugia;
+}());
 var CorteLinea = /** @class */ (function () {
     // puntos:Array<>;
-    function CorteLinea(inicio, final, distancia) {
-        this.inicio = inicio;
-        this.final = final;
-        this.distanacia = distancia;
+    function CorteLinea(lienzo, dis) {
+        var _this = this;
+        this.color = "#EF3838";
+        this.dis = dis;
         this.puntos = new Array();
         this.historial = {};
-        var nPuntos = final.y - inicio.y;
+        this.lienzo = lienzo;
+        this.registrando = false;
         this.linea = new createjs.Shape();
+        this.linea.on("mousedown", function () {
+            _this.registrando = true;
+            _this.lienzo.cortando = true;
+        });
+        lienzo.sala.on("stagemouseup", function () {
+            if (_this.registrando) {
+                console.log("Des" + _this.getDesviacion());
+            }
+            _this.registrando = false;
+            _this.lienzo.cortando = false;
+        });
+        lienzo.sala.on("stagemousemove", function () {
+            if (_this.registrando) {
+                var y = Math.round(_this.lienzo.sala.mouseY);
+                var x = Math.round(_this.lienzo.sala.mouseX);
+                _this.historial[y] = x;
+            }
+        });
+    }
+    CorteLinea.prototype.trazoCurva = function (inicio, final, distancia, dist) {
+        var nPuntos = final.y - inicio.y;
+        var dis = dist;
         for (var i = 0; i < nPuntos; i++) {
-            var x = Math.round(this.inicio.x + Math.sin(radians(i)) * distancia);
-            var y = Math.round(this.inicio.y + i);
-            this.linea.graphics.beginFill("red").drawCircle(x, y, 3);
+            var x = Math.round(inicio.x + Math.sin(radians(i * dis)) * distancia);
+            var y = Math.round(inicio.y + (i));
+            if (i % this.dis == 0) {
+                this.linea.graphics.beginFill(this.color).drawCircle(x, y, 3);
+            }
             this.puntos.push({ x: x, y: y });
         }
-        this.linea.graphics.beginFill("blue").drawCircle(this.inicio.x, this.inicio.y, 3);
-    }
+        this.linea.graphics.beginFill("blue").drawCircle(inicio.x, inicio.y, 5);
+    };
+    CorteLinea.prototype.trazadoLineal = function (inicio, final) {
+        var nPuntos = final.y - inicio.y;
+        var m = 0;
+        if ((final.x - inicio.x) == 0) {
+        }
+        else {
+            m = (final.y - inicio.y) / (final.x - inicio.x);
+        }
+        for (var i = 0; i < nPuntos; i++) {
+            var y = Math.round(i + inicio.y);
+            var x = inicio.x;
+            if (m != 0) {
+                x = (y - inicio.y + (m * inicio.x)) / m;
+            }
+            if (i % this.dis == 0) {
+                this.linea.graphics.beginFill(this.color).drawCircle(x, y, 3);
+            }
+            this.puntos.push({ x: x, y: y });
+        }
+        this.linea.graphics.beginFill("blue").drawCircle(inicio.x, inicio.y, 5);
+        this.linea.graphics.beginFill("blue").drawCircle(final.x, final.y, 5);
+    };
+    CorteLinea.prototype.trazadoCurvaDerecha = function (inicio, final) {
+        var distancia = final.y - inicio.y;
+        for (var i = 0; i < distancia; i++) {
+            var index = i / distancia * 90;
+            var x = Math.round(inicio.x + Math.cos(radians(index)) * distancia);
+            var y = Math.round(inicio.y + Math.sin(radians(index)) * distancia);
+            this.linea.graphics.beginFill(this.color).drawCircle(x, y, 3);
+            this.puntos.push({ x: x, y: y });
+        }
+        this.linea.graphics.beginFill("blue").drawCircle(inicio.x, inicio.y, 5);
+        this.linea.graphics.beginFill("blue").drawCircle(final.x, final.y, 5);
+    };
+    CorteLinea.prototype.trazadoCurvaIzquierda = function (inicio, final) {
+        var distancia = final.y - inicio.y;
+        for (var i = 0; i < distancia; i++) {
+            var index = i / distancia * 90;
+            var x = Math.round(inicio.x + -Math.cos(radians(index)) * distancia);
+            var y = Math.round(inicio.y + Math.sin(radians(index)) * distancia);
+            if (i % this.dis == 0) {
+                this.linea.graphics.beginFill(this.color).drawCircle(x, y, 3);
+            }
+            this.puntos.push({ x: x, y: y });
+        }
+        this.linea.graphics.beginFill("blue").drawCircle(inicio.x, inicio.y, 5);
+        this.linea.graphics.beginFill("blue").drawCircle(final.x, final.y, 5);
+    };
     CorteLinea.prototype.getDesviacion = function () {
         var matrix = [];
         var total_matrix = 0;
@@ -46,42 +163,17 @@ var CorteLinea = /** @class */ (function () {
     };
     return CorteLinea;
 }());
-var canvas = document.createElement('canvas');
-canvas.width = 1280;
-canvas.height = 720;
-var lienzo = new createjs.Stage(canvas);
-var corte = new CorteLinea({ x: 600, y: 100 }, { x: 600, y: 600 }, 80);
-var val = false;
-lienzo.on("stagemousedown", function () {
-    val = true;
-    console.log("true");
-});
-lienzo.on("stagemouseup", function () {
-    val = false;
-});
-lienzo.on("stagemousemove", function () {
-    if (val) {
-        corte.historial[Math.round(lienzo.mouseY)] = Math.round(lienzo.mouseX);
-        console.log(corte.historial[Math.round(lienzo.mouseY)]);
-    }
-});
-createjs.Ticker.addEventListener("tick", function () {
-    if (val) {
-        corte.historial[Math.round(lienzo.mouseY)] = Math.round(lienzo.mouseX);
-        console.log(corte.historial[Math.round(lienzo.mouseY)]);
-    }
-});
-lienzo.addChild(corte.linea);
-lienzo.update();
-$(".principal").append(canvas);
-/*
-               let x = Math.round(this.inicio.x + Math.sin(radians(i)) * distancia);
-               let y = Math.round(this.inicio.y + i);
-               
-
-            index++;
-
-            if(Math.cos(radians(index)) < 1){
-                //dis = Math.round(this.inicio.y + Math.cos(radians(index)) * distancia) + distancia + dis;
-                //index = 0;
-            }*/ 
+function cargarSalaCirugias() {
+    var sala = new salaCirugia();
+    sala.cargarCuerpo("../img/cortes/CuerpoB.png");
+    var corteA = new CorteLinea(sala, 20);
+    corteA.trazadoLineal({ x: 500, y: 100 }, { x: 500, y: 430 });
+    var corteB = new CorteLinea(sala, 10);
+    corteB.trazadoLineal({ x: 400, y: 350 }, { x: 470, y: 430 });
+    var corteC = new CorteLinea(sala, 10);
+    corteC.trazadoLineal({ x: 600, y: 350 }, { x: 530, y: 430 });
+    sala.sala.addChild(corteA.linea, corteB.linea, corteC.linea);
+    sala.sala.update();
+    $(".principal").append(sala.canvas);
+}
+cargarSalaCirugias();
--- app/typescript/cortes.ts
+++ app/typescript/cortes.ts
@@ -25,39 +25,198 @@ interface CoordenadaP {
 }
 
 
+class salaCirugia {
+
+    sala: createjs.Stage;
+    cortando: boolean;
+    canvas: HTMLCanvasElement;
+    bisturi?:createjs.Bitmap;
+
+    constructor() {
+        this.canvas = document.createElement('canvas');
+        this.sala = new createjs.Stage(this.canvas);
+        this.cortando = false;
+        this.canvas.width = 1000;
+        this.canvas.height = 480;
+        this.canvas.style.background = "#AEE9DB";
+
+        let bisturi_img = document.createElement('img');
+        bisturi_img.src = "../img/cortes/bisturi.png";
+    
+        let corte = new createjs.Shape();
+
+        this.sala.addChild(corte);
+
+        bisturi_img.addEventListener("load", () => {
+            this.bisturi = new createjs.Bitmap(bisturi_img);
+            this.sala.addChild(this.bisturi);
+
+                this.sala.on("stagemousemove", () => {
+                    let x = this.sala.mouseX;
+                    let y = this.sala.mouseY;
+                    if(this.bisturi != null){
+                    this.bisturi.x = x - 5;
+                    this.bisturi.y = y;
+                    }
+                    if (this.cortando){
+                        corte.graphics.beginFill("red").drawCircle(x, y, 1);
+                    }  
+                    this.sala.update();
+                });
+
+           
+        });
+    }
+
+    cargarCuerpo(url: string) {
+        let cuerpoA = document.createElement('img');
+        cuerpoA.src = url;
+
+        cuerpoA.addEventListener("load", () => {
+            let ima = new createjs.Bitmap(cuerpoA);
+            let cor = ima.getBounds();
+            ima.x = (this.canvas.width - cor.width) / 2;
+            this.sala.addChildAt(ima, 0);
+            this.sala.update();
+        });
+    }
+}
+
 
 class CorteLinea {
 
-    inicio: Coordenada;
-    final: Coordenada;
-    distanacia: number;
     linea: createjs.Shape;
     puntos: Array<Coordenada>;
     historial: any;
+    lienzo: salaCirugia;
+    registrando: boolean
+    dis: number;
+    color = "#EF3838";
+
     // puntos:Array<>;
 
-    constructor(inicio: Coordenada, final: Coordenada, distancia: number) {
-        this.inicio = inicio;
-        this.final = final;
-        this.distanacia = distancia;
+    constructor(lienzo: salaCirugia, dis: number) {
+        this.dis = dis;
         this.puntos = new Array();
         this.historial = {};
+        this.lienzo = lienzo;
+        this.registrando = false;
 
-        let nPuntos = final.y - inicio.y;
 
         this.linea = new createjs.Shape();
 
+        this.linea.on("mousedown", () => {
+            this.registrando = true;
+            this.lienzo.cortando = true;
+
+        });
+
+        lienzo.sala.on("stagemouseup", () => {
+            if (this.registrando) {
+                console.log("Des" + this.getDesviacion());
+            }
+            this.registrando = false;
+            this.lienzo.cortando = false;
+        });
+
+        lienzo.sala.on("stagemousemove", () => {
+            if (this.registrando) {
+                let y = Math.round(this.lienzo.sala.mouseY);
+                let x = Math.round(this.lienzo.sala.mouseX);
+                this.historial[y] = x;
+            }
+        });
+    }
+
+    trazoCurva(inicio: Coordenada, final: Coordenada, distancia: number, dist: number) {
+
+        let nPuntos = final.y - inicio.y;
+        let dis = dist;
+
         for (let i = 0; i < nPuntos; i++) {
 
-            let x = Math.round(this.inicio.x + Math.sin(radians(i)) * distancia);
-               let y = Math.round(this.inicio.y + i);
+            let x = Math.round(inicio.x + Math.sin(radians(i * dis)) * distancia);
+            let y = Math.round(inicio.y + (i));
 
-            this.linea.graphics.beginFill("red").drawCircle(x, y, 3);
+            if (i % this.dis == 0) {
+                this.linea.graphics.beginFill(this.color).drawCircle(x, y, 3);
+            }
             this.puntos.push({ x: x, y: y });
 
         }
 
-        this.linea.graphics.beginFill("blue").drawCircle(this.inicio.x, this.inicio.y, 3);
+        this.linea.graphics.beginFill("blue").drawCircle(inicio.x, inicio.y, 5);
+
+    }
+
+    trazadoLineal(inicio: Coordenada, final: Coordenada) {
+
+        let nPuntos = final.y - inicio.y;
+
+        let m = 0;
+        if ((final.x - inicio.x) == 0) {
+
+        } else {
+            m = (final.y - inicio.y) / (final.x - inicio.x);
+        }
+
+
+        for (let i = 0; i < nPuntos; i++) {
+
+            let y = Math.round(i + inicio.y);
+            let x = inicio.x;
+            if (m != 0) {
+                x = (y - inicio.y + (m * inicio.x)) / m;
+            }
+
+            if (i % this.dis == 0) {
+                this.linea.graphics.beginFill(this.color).drawCircle(x, y, 3);
+            }
+            this.puntos.push({ x: x, y: y });
+        }
+
+        this.linea.graphics.beginFill("blue").drawCircle(inicio.x, inicio.y, 5);
+        this.linea.graphics.beginFill("blue").drawCircle(final.x, final.y, 5);
+
+    }
+
+    trazadoCurvaDerecha(inicio: Coordenada, final: Coordenada) {
+
+        let distancia = final.y - inicio.y;
+
+        for (let i = 0; i < distancia; i++) {
+
+            let index = i / distancia * 90;
+
+            let x = Math.round(inicio.x + Math.cos(radians(index)) * distancia);
+            let y = Math.round(inicio.y + Math.sin(radians(index)) * distancia);
+
+            this.linea.graphics.beginFill(this.color).drawCircle(x, y, 3);
+            this.puntos.push({ x: x, y: y });
+        }
+        this.linea.graphics.beginFill("blue").drawCircle(inicio.x, inicio.y, 5);
+        this.linea.graphics.beginFill("blue").drawCircle(final.x, final.y, 5);
+    }
+
+    trazadoCurvaIzquierda(inicio: Coordenada, final: Coordenada) {
+
+        let distancia = final.y - inicio.y;
+
+        for (let i = 0; i < distancia; i++) {
+
+            let index = i / distancia * 90;
+
+            let x = Math.round(inicio.x + -Math.cos(radians(index)) * distancia);
+            let y = Math.round(inicio.y + Math.sin(radians(index)) * distancia);
+
+            if (i % this.dis == 0) {
+                this.linea.graphics.beginFill(this.color).drawCircle(x, y, 3);
+            }
+            this.puntos.push({ x: x, y: y });
+        }
+        this.linea.graphics.beginFill("blue").drawCircle(inicio.x, inicio.y, 5);
+        this.linea.graphics.beginFill("blue").drawCircle(final.x, final.y, 5);
+
     }
 
     getDesviacion() {
@@ -78,56 +237,29 @@ class CorteLinea {
     }
 }
 
-let canvas = document.createElement('canvas');
-canvas.width = 1280;
-canvas.height = 720
-let lienzo = new createjs.Stage(canvas);
-
-let corte = new CorteLinea({ x: 600, y: 100 }, { x: 600, y: 600 }, 80);
-
-let val = false;
-lienzo.on("stagemousedown", () => {
-    val = true;
-    console.log("true");
-});
-
-lienzo.on("stagemouseup", () => {
-    val = false;
-});
-
-lienzo.on("stagemousemove", () => {
-
-    if (val) {
-        corte.historial[Math.round(lienzo.mouseY)] = Math.round(lienzo.mouseX);
-        console.log(corte.historial[Math.round(lienzo.mouseY)]);
-    }
-
-});
-
-createjs.Ticker.addEventListener("tick", function () {
-
-    if (val) {
-        corte.historial[Math.round(lienzo.mouseY)] = Math.round(lienzo.mouseX);
-        console.log(corte.historial[Math.round(lienzo.mouseY)]);
-    }
-
-});
-
-lienzo.addChild(corte.linea);
-lienzo.update();
 
 
-$(".principal").append(canvas);
+function cargarSalaCirugias() {
 
 
-/*   
-               let x = Math.round(this.inicio.x + Math.sin(radians(i)) * distancia);
-               let y = Math.round(this.inicio.y + i);
-               
+    let sala = new salaCirugia();
+  
+    sala.cargarCuerpo("../img/cortes/CuerpoB.png");
 
-            index++;
+    let corteA = new CorteLinea(sala, 20);
+    corteA.trazadoLineal({ x: 500, y: 100 }, { x: 500, y: 430 });
 
-            if(Math.cos(radians(index)) < 1){
-                //dis = Math.round(this.inicio.y + Math.cos(radians(index)) * distancia) + distancia + dis;
-                //index = 0;
-            }*/
+    let corteB = new CorteLinea(sala, 10);
+    corteB.trazadoLineal({ x: 400, y: 350 }, { x: 470, y: 430 });
+
+    let corteC = new CorteLinea(sala, 10);
+    corteC.trazadoLineal({ x: 600, y: 350 }, { x: 530, y: 430 });
+
+    sala.sala.addChild(corteA.linea, corteB.linea, corteC.linea);
+    sala.sala.update();
+
+
+    $(".principal").append(sala.canvas);
+}
+
+cargarSalaCirugias();
