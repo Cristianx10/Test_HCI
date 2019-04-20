140a3a782d2ecb5252d473be6db0cf8547994e52
Cristian Salguero <david_salinasx10@hotmail.com>
2019-04-20 16:09:22 -0500

    timer

--- app/typescript/main.ts
+++ app/typescript/main.ts
@@ -8,28 +8,28 @@ function degrees(radians: number) {
     return radians * 180 / Math.PI;
 };
 
-function shuffle(array:any) {
+function shuffle(array: any) {
     array.sort(function () { return Math.random() - 0.5; });
 }
 
-class Resultados{
+class Resultados {
     categorias: any = {};
-    pruebas:any = [];
+    pruebas: any = [];
 
-    constructor(){
+    constructor() {
         localStorage.clear();
-       
-        if(this.categorias == null){
+
+        if (this.categorias == null) {
             this.categorias = {};
         }
     }
 
-    resultados(pru:any){
+    resultados(pru: any) {
         this.pruebas.push(pru);
     }
 
-    sumar(nombre:string, valor:number){
-        if(this.categorias[nombre] == null){
+    sumar(nombre: string, valor: number) {
+        if (this.categorias[nombre] == null) {
             this.categorias[nombre] = 0;
         }
         this.categorias[nombre] += valor;
@@ -37,98 +37,172 @@ class Resultados{
 
 }
 
-var RESULTADO:Resultados = new Resultados();
+var RESULTADO: Resultados = new Resultados();
 
-interface Validable{
-    getElementosHTML():Array<HTMLElement>;
+interface Validable {
+    getElementosHTML(): Array<HTMLElement>;
 }
 
-class Navegable{
+class Navegable {
 
-    elementos:Contenedor;
-    secciones:Array<HTMLElement>;
-    actual:number;
-    progreso:Progress;
+    elementos: Contenedor;
+    secciones: Array<HTMLElement>;
+    actual: number;
+    progreso: Progress;
+    tiempo: HTMLElement;
+    avance: HTMLElement;
+    private ti: HTMLElement;
+    private av: HTMLElement;
 
-    constructor(elementos:Contenedor){
+    constructor(elementos: Contenedor) {
         this.elementos = elementos;
         this.progreso = new Progress(elementos.elementos.length, 0);
-        
+
         this.elementos.elementos.forEach(e => {
             e.tiempoDefinido = true;
-            e.setTiempo(()=>{
-                if(e.tiempoDefinido == false){
+            
+            e.setTiempo(() => {
+                if (e.tiempoDefinido == false) {
                     this.siguiente();
                 }
+                this.siguiente();
+            });
+
+            e.timer.setProgreso((m: number, s: number) => {
+                if(e ==  this.elementos.elementos[this.actual]){
+                    if (s < 10 && s < 60 && s> -1 && m < 59) {
+                        this.ti.innerText = "0" + m + ":0" + s;
+                    } else {
+                        this.ti.innerText = "0" + m + ":" + s;
+                    }
+                }
+                
             });
         });
 
         this.secciones = elementos.getElementosHTML();
         this.actual = 0;
         this.secciones.forEach((s, i) => {
-            if(i == 0){
+            if (i == 0) {
                 s.style.display = "block";
-            }else{
+            } else {
                 s.style.display = "none";
             }
         });
 
         this.actualPantalla().start();
+
+        this.tiempo = document.createElement('div');
+        this.tiempo.className = "nav_tiempo"
+        let c = document.createElement('div');
+        c.className = "conteo";
+
+        let c_ima = document.createElement('img');
+        c_ima.className = "conteo__relog";
+        c_ima.src = "../../img/relog.png";
+
+        this.ti = document.createElement('p');
+        this.ti.className = "conteo__p";
+        c.append(c_ima, this.ti);
+        this.tiempo.append(c);
+
+        this.avance = document.createElement('div');
+        this.avance.className = "nav_avance";
+
+        let a = document.createElement('div');
+        a.className = "avance";
+
+        this.av = document.createElement('p');
+        this.av.className = "avance__p";
+
+        this.avance.append(a);
+        a.append(this.av);
+
+        this.av.innerText = this.actual + 1 + "/" + this.secciones.length;
+        this.ti.innerText = "0:00";
     }
 
-    actualObjeto() : any{
+    getTiempoHTML() {
+        return this.tiempo;
+    }
+
+    getAvanceHTML() {
+        return this.avance;
+    }
+
+    colocarTiempo() {
+        $(".principal").append(this.tiempo);
+        this.tiempo.style.position = "absolute";
+        this.tiempo.style.top = "20px";
+        this.tiempo.style.right = "40px";
+
+
+    }
+
+    colocarAvance() {
+
+        $(".principal").append(this.avance);
+        this.avance.style.position = "absolute";
+        this.avance.style.left = "0px";
+
+    }
+
+    actualObjeto(): any {
         return this.elementos.elementos[this.actual].getObjeto();
     }
 
-    actualPantalla() : ContenidoA{
+    actualPantalla(): ContenidoA {
         return this.elementos.elementos[this.actual];
     }
 
-    asignarCondiciones(recorrer:Function){
+    asignarCondiciones(recorrer: Function) {
         this.elementos.elementos.forEach(e => {
-            recorrer(e.objeto);    
+            recorrer(e.objeto);
         });
     }
 
-    getElement(){
+    getElement() {
         return this.secciones;
     }
 
-    mostrar(seccion:HTMLElement){
+    mostrar(seccion: HTMLElement) {
         seccion.style.display = "block";
     }
 
-    ocultar(seccion:HTMLElement){
+    ocultar(seccion: HTMLElement) {
         seccion.style.display = "none";
     }
 
-    siguiente(accion?:Function, final?:Function):void{
+    siguiente(accion?: Function, final?: Function): void {
         this.ocultar(this.secciones[this.actual]);
-        if(this.actual < this.secciones.length-1){
-            if(accion){
+        if (this.actual < this.secciones.length - 1) {
+            if (accion) {
                 accion(this.actualPantalla(), this.actual);
             }
             this.actualPantalla().tiempoDefinido = true;
+            this.elementos.elementos[this.actual].timer.stop();
+          
             this.actual++;
+            this.av.innerText = this.actual + 1 + "/" + this.secciones.length;
             this.progreso.actualizarPosicion(this.actual);
             this.mostrar(this.secciones[this.actual]);
             this.actualPantalla().start();
-        }else{
-            if(final){
+        } else {
+            if (final) {
                 final();
             }
-        }      
+        }
     }
 }
 
-class Progress{
-    contenedor:HTMLElement;
-    progress:HTMLProgressElement;
-    indice:HTMLElement;
-    total:number;
-    actual:number;
+class Progress {
+    contenedor: HTMLElement;
+    progress: HTMLProgressElement;
+    indice: HTMLElement;
+    total: number;
+    actual: number;
 
-    constructor(  total:number, inicial:number){
+    constructor(total: number, inicial: number) {
         this.total = total;
         this.actual = inicial;
         this.contenedor = document.createElement('div');
@@ -142,39 +216,40 @@ class Progress{
 
         this.progress.value = inicial;
         this.progress.max = total;
-        this.indice.innerText = inicial+"";
+        this.indice.innerText = inicial + "";
 
         this.contenedor.append(con_contendor);
         con_contendor.append(this.progress, this.indice);
         this.actualizarPosicion(this.actual);
     }
 
-    actualizarPosicion(ini:number){
+    actualizarPosicion(ini: number) {
         let maximo = 550;
-        let actual = maximo*ini/this.total;
+        let actual = maximo * ini / this.total;
         this.indice.style.left = actual + "px";
         this.progress.value = ini;
-        this.indice.innerText = ini+"";
+        this.indice.innerText = ini + "";
         this.actual = ini;
     }
 
-    getElemento(){
+    getElemento() {
         return this.contenedor;
     }
 }
 
-class PantallaHTML{
-    elemento:HTMLElement;
+class PantallaHTML {
+    elemento: HTMLElement;
 
-    constructor(elemento:HTMLElement){
+    constructor(elemento: HTMLElement) {
         this.elemento = elemento;
     }
-    getElemento(){
+
+    getElemento() {
         return this.elemento;
     }
 }
 
-function toPantallas(pantallas:Array<HTMLElement>){
+function toPantallas(pantallas: Array<HTMLElement>) {
 
     let contenido = [];
     for (let i = 0; i < pantallas.length; i++) {
@@ -187,100 +262,101 @@ function toPantallas(pantallas:Array<HTMLElement>){
     return contenedorPadre;
 }
 
-class Contenedor implements Validable{
+class Contenedor implements Validable {
 
-    elementos:Array<ContenidoA>;
+    elementos: Array<ContenidoA>;
 
-    constructor(elementos:Array<ContenidoA>){
+    constructor(elementos: Array<ContenidoA>) {
         this.elementos = elementos;
     }
 
-    foreachElementos(elemento:HTMLElement){
+    foreachElementos(elemento: HTMLElement) {
         this.elementos.forEach(e => {
             elemento.appendChild(e.elementoHTML);
         });
     }
 
-    getObjectIndex(index:number){
-        let ElementoHTML:Object = this.elementos[index].getObjeto();
+    getObjectIndex(index: number) {
+        let ElementoHTML: Object = this.elementos[index].getObjeto();
         return ElementoHTML;
     }
 
-    getElementosHTML(): Array<HTMLElement>{
-        let ElementosHTML:Array<HTMLElement> = new Array();
+    getElementosHTML(): Array<HTMLElement> {
+        let ElementosHTML: Array<HTMLElement> = new Array();
         this.elementos.forEach(e => {
             ElementosHTML.push(e.getElementoHTML());
         });
         this.elementos;
         return ElementosHTML;
     }
-    
+
 }
 
-class ContenidoA{
+class ContenidoA {
 
-    objeto:Object;
-    elementoHTML:HTMLElement;
-    timer:Timer;
-    minutos?:number;
-    segundos?:number;
-    tiempoDefinido:boolean;
+    objeto: Object;
+    elementoHTML: HTMLElement;
+    timer: Timer;
+    minutos?: number;
+    segundos?: number;
+    tiempoDefinido: boolean;
 
-    constructor(elementoHTML:HTMLElement, objeto:Object, minutos?:number, segundos?:number){
+    constructor(elementoHTML: HTMLElement, objeto: Object, minutos?: number, segundos?: number) {
         this.elementoHTML = elementoHTML;
         this.objeto = objeto;
         this.timer = new Timer();
         this.tiempoDefinido = false;
 
-        if(segundos != null){
+        if (segundos != null) {
             this.minutos = minutos;
             this.segundos = segundos;
-         
-        }else if(minutos !=null){
+
+        } else if (minutos != null) {
             this.segundos = minutos;
             this.minutos = 0;
         }
-        
+
     }
 
-    tiempo(minutos?:number, segundos?:number){
+    tiempo(minutos?: number, segundos?: number) {
         this.minutos = minutos;
         this.segundos = segundos;
     }
-    
-    start(){
-        if(this.minutos != null && this.segundos != null){
+
+    start() {
+        if (this.minutos != null && this.segundos != null) {
             this.timer.startTempo(this.minutos, this.segundos);
         }
     }
-    
 
-    setTiempo(termino:Function){
+
+    setTiempo(termino: Function) {
         this.timer.termino = termino;
+        this.tiempoDefinido = true;
     }
 
-    getElementoHTML(){
+    getElementoHTML() {
         return this.elementoHTML;
     }
 
-    getObjeto(){
+    getObjeto() {
         return this.objeto;
     }
 
 }
 
 
-function loadJson(ruta:string, result:Function){
+function loadJson(ruta: string, result: Function) {
     let valor;
     let carga = new createjs.LoadQueue();
     carga.loadFile(ruta);
-    carga.on("fileload",(e:any)=>{
+    carga.on("fileload", (e: any) => {
 
         valor = e.result;
         result(valor);
 
     });
-    
+
 }
 
 
@@ -298,20 +374,20 @@ function crearMatrix(colum: number, fil: number, wid: number, hei: number) {
 
 
     for (let i = 0; i < length; i++) {
-      columna++;
-      let posx = columna * width;
-      let posy = fila;
-      if ((columna + 1) == columnas) {
-        columna = -1;
-        fila += height;
-      }
-      let pos = { x: posx, y: posy, width: width, height: height };
-      arreglo.push(pos);
+        columna++;
+        let posx = columna * width;
+        let posy = fila;
+        if ((columna + 1) == columnas) {
+            columna = -1;
+            fila += height;
+        }
+        let pos = { x: posx, y: posy, width: width, height: height };
+        arreglo.push(pos);
     }
     return arreglo;
-  }
+}
 
-  function cargarImagen(url: string, width: number, height: number, columnas: number, filas: number) {
+function cargarImagen(url: string, width: number, height: number, columnas: number, filas: number) {
 
     let imagenes = new Array<HTMLElement>();
     let c = -1;
@@ -319,8 +395,8 @@ function crearMatrix(colum: number, fil: number, wid: number, hei: number) {
 
     let image = document.createElement("div");
     image.style.backgroundImage = `url(${url})`; // load the image
-    image.style.width = width*columnas + "px";
-    image.style.height = height*filas + "px";
+    image.style.width = width * columnas + "px";
+    image.style.height = height * filas + "px";
 
     image.style.position = "absolute";
 
@@ -328,38 +404,38 @@ function crearMatrix(colum: number, fil: number, wid: number, hei: number) {
     let total = filas * columnas;
     for (let i = 0; i < total; i++) {
 
-      let contenedor = document.createElement('div');
-      contenedor.style.position = "relative";
-      contenedor.style.width = width + "px";
-      contenedor.style.height = height + "px";
-      contenedor.style.overflow = "hidden";
+        let contenedor = document.createElement('div');
+        contenedor.style.position = "relative";
+        contenedor.style.width = width + "px";
+        contenedor.style.height = height + "px";
+        contenedor.style.overflow = "hidden";
 
-      let fragmentoImg: HTMLElement = <any>image.cloneNode();
+        let fragmentoImg: HTMLElement = <any>image.cloneNode();
 
-      c++;
-      fragmentoImg.style.left = -(c * width) + "px";
-      fragmentoImg.style.top = f + "px";
+        c++;
+        fragmentoImg.style.left = -(c * width) + "px";
+        fragmentoImg.style.top = f + "px";
 
-      if ((c + 1) == columnas) {
-        c = -1;
-        f -= height;
-      }
+        if ((c + 1) == columnas) {
+            c = -1;
+            f -= height;
+        }
 
-      contenedor.appendChild(fragmentoImg);
-      imagenes.push(contenedor);
+        contenedor.appendChild(fragmentoImg);
+        imagenes.push(contenedor);
     }
 
     return imagenes;
-  }
+}
 
 
-function irA(url:string){
+function irA(url: string) {
     $(".principal").load(url);
 }
 
 
 
-function askConfirmation (evt:any) {
+function askConfirmation(evt: any) {
     var msg = 'Si recarga la página perdera todos los datos ingresados.\n¿Deseas recargar la página?';
     evt.returnValue = msg;
     return msg;
@@ -367,84 +443,84 @@ function askConfirmation (evt:any) {
 
 
 
-function random(min:number, max:number) {
+function random(min: number, max: number) {
     return Math.round(Math.random() * (max - min) + min);
-  }
+}
 
-  function hsvToRgb(h, s, v) {
+function hsvToRgb(h, s, v) {
     var r, g, b;
     var i;
     var f, p, q, t;
-     
+
     // Make sure our arguments stay in-range
     h = Math.max(0, Math.min(360, h));
     s = Math.max(0, Math.min(100, s));
     v = Math.max(0, Math.min(100, v));
-     
+
     // We accept saturation and value arguments from 0 to 100 because that's
     // how Photoshop represents those values. Internally, however, the
     // saturation and value are calculated from a range of 0 to 1. We make
     // That conversion here.
     s /= 100;
     v /= 100;
-     
-    if(s == 0) {
+
+    if (s == 0) {
         // Achromatic (grey)
         r = g = b = v;
         return [
-            Math.round(r * 255), 
-            Math.round(g * 255), 
+            Math.round(r * 255),
+            Math.round(g * 255),
             Math.round(b * 255)
         ];
     }
-     
+
     h /= 60; // sector 0 to 5
     i = Math.floor(h);
     f = h - i; // factorial part of h
     p = v * (1 - s);
     q = v * (1 - s * f);
     t = v * (1 - s * (1 - f));
-     
-    switch(i) {
+
+    switch (i) {
         case 0:
             r = v;
             g = t;
             b = p;
             break;
-     
+
         case 1:
             r = q;
             g = v;
             b = p;
             break;
-     
+
         case 2:
             r = p;
             g = v;
             b = t;
             break;
-     
+
         case 3:
             r = p;
             g = q;
             b = v;
             break;
-     
+
         case 4:
             r = t;
             g = p;
             b = v;
             break;
-     
+
         default: // case 5:
             r = v;
             g = p;
             b = q;
     }
-     
+
     return [
-        Math.round(r * 255), 
-        Math.round(g * 255), 
+        Math.round(r * 255),
+        Math.round(g * 255),
         Math.round(b * 255)
     ];
 }
@@ -473,15 +549,15 @@ setValidacion(validacion:Function){
 
     //Implementacion
     con_tablero.getObjectIndex(nav.actual).setValidacion(()=>{
-        
+
     });
 
     con_tablero.getObjectIndex(nav.actual).setIntentoAcierto(()=>{
-        
+
     });
 
     con_tablero.getObjectIndex(nav.actual).setIntentoFallo(()=>{
-        
+
     });
 
 
