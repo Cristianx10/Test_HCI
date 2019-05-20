c3e226ed2590e47b12698c822e0cddeb5aefbb42
Cristian Salguero <david_salinasx10@hotmail.com>
2019-05-20 08:57:17 -0500

    Fobias

--- app/typescript/descartes.ts
+++ app/typescript/descartes.ts
@@ -13,7 +13,7 @@ class Descartes extends Interaccion {
         this.elemento.append(imagen);
     }
 
-    validar() {
+    valida() {
         let cont = 0;
         this.imagenes.forEach(i => {
             if (i.validado) {
@@ -44,11 +44,14 @@ class Descarte_elemento {
 
         this.elemento.addEventListener("click", () => {
 
+
+
             if (this.valor) {
                 if (this.tablero.intentoAcierto != null) {
                     this.tablero.intentoAcierto();
                 }
-                console.log("acierto");
+                this.tablero.aciertos++;
+                this.tablero.intentos++;
             }
 
 
@@ -56,17 +59,20 @@ class Descarte_elemento {
                 if (this.tablero.intentoFallo != null) {
                     this.tablero.intentoFallo();
                 }
-                console.log("fallo");
+                tablero.fallos++;
+                this.tablero.intentos++;
+               
             }
 
             this.validado = true;
 
-            if (this.tablero.validar()) {
+            if (this.tablero.valida()) {
                 if (this.tablero.validacion != null) {
                     this.tablero.validacion();
                 }
-                console.log("Gano");
+                tablero.valido = true;    
             }
+
             this.ocultar();
            
         });
@@ -76,37 +82,5 @@ class Descarte_elemento {
         this.elemento.style.display = "none";
     }
 }
-
-let c = matrixFija("../img/arte/ojoveloz1.png", 100, 50, 5, 5);
-let descartes = new Descartes();
-
-descartes.agregar(c[0], false);
-descartes.agregar(c[1], true);
-descartes.agregar(c[2], false);
-descartes.agregar(c[3], false);
-descartes.agregar(c[4], false);
-descartes.agregar(c[5], false);
-descartes.agregar(c[6], false);
-descartes.agregar(c[7], true);
-descartes.agregar(c[8], false);
-descartes.agregar(c[9], false);
-descartes.agregar(c[10], false);
-descartes.agregar(c[11], true);
-descartes.agregar(c[12], false);
-descartes.agregar(c[13], false);
-descartes.agregar(c[14], false);
-descartes.agregar(c[15], false);
-descartes.agregar(c[16], false);
-descartes.agregar(c[17], true);
-descartes.agregar(c[18], false);
-descartes.agregar(c[19], false);
-descartes.agregar(c[20], false);
-descartes.agregar(c[21], false);
-descartes.agregar(c[22], false);
-descartes.agregar(c[23], false);
-descartes.agregar(c[24], false);
-
-
-descartes.incluirEn(".principal");
 
 
